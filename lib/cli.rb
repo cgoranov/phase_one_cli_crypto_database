0980d64f81c4508a0d7e6615b4cdb7f80e492b97
Che <cgoranov@gmail.com>
2021-03-29 19:35:24 -0500

    built out search method and control flow

--- lib/cli.rb
+++ lib/cli.rb
@@ -8,23 +8,34 @@ class CLI
     end
 
     def menu
-        puts "Would you like to see the top 5 largest cryptocurrencies in the world?"
         puts "Enter 'Yes' to see our list or 'Exit' to leave search"
         user_input
         if valid_input?(user_input)
             if user_input == "Yes"
                 crypto_currency_list
+                search
+            else
+                puts "We are sorry to see you go. Comeback anytime for all your Crypto Data needs!"
+            end
 
         else
             "Invalid input"
+            menu
+        end
     end
 
     def search
+        crypto_currency_list
+        puts " "
+        puts "Please enter name (yes case sensitive!) of the currency you would like to see more detail about."
+        puts "You can type 'Exit' to exit application"
+        user_input
     
     end
 
     def greeting
         puts "Welcome to Crypto Search!"
+        puts "Would you like to see the top 5 largest cryptocurrencies in the world?"
     end
 
 
